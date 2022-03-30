c451a623e198e9fa895259fcab9ba37c46d4cbb1
Michaela DeCamara <michaela.decamara@gmail.com>
2022-03-29 23:46:22 -0400

    fixed routes

--- routes/product.js
+++ routes/product.js
@@ -1,38 +1,39 @@
 const Product = require("../models/Product");
-const { 
+const {
   verifyToken,
   verifyTokenAndAuthorization,
-  verifyTokenAndAdmin, 
+  verifyTokenAndAdmin,
 } = require("./verifyToken");
 
 const router = require("express").Router();
 
-// CREATE 
-router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = new Product(rwq.body)
+//CREATE
 
-  try{
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
     const savedProduct = await newProduct.save();
     res.status(200).json(savedProduct);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// UPDATE
+//UPDATE
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-    try {
-      const updatedProduct = await Product.findByIdAndUpdate(
-        req.params.id, 
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
       {
         $set: req.body,
       },
-      { new:true }
-      );
-      res.status(200).json(updatedProduct);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //DELETE
--- routes/user.js
+++ routes/user.js
@@ -7,38 +7,6 @@ const {
 
 const router = require("express").Router();
 
-// UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  if(req.body.password) {
-   req.body.password = CryptoJS.AES.encrypt(
-     req.body.password,
-     process.env.PASS_SEC
-   ).toString();
-  }
-
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id, 
-      {
-        $set: req.body,
-      },
-      { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
-
-// DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  try{
-    const user = await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("User has been deleted...");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // GET USER
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
@@ -57,7 +25,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
     const users = query 
-      ? await User.find().sort({ _id: -1 }).limit(1) 
+      ? await User.find().sort({ _id: -1 }).limit(5) 
       : await User.find();  
     res.status(200).json(user);
   } catch (err) {
@@ -92,5 +60,39 @@ router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if(req.body.password) {
+   req.body.password = CryptoJS.AES.encrypt(
+     req.body.password,
+     process.env.PASS_SEC
+   ).toString();
+  }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id, 
+      {
+        $set: req.body,
+      },
+      { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try{
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
